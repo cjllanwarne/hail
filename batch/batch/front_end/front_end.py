337857dd5fecda01b4b7cf4fd4f1f88bd0f0a252
Chris Llanwarne <cjllanwarne@gmail.com>
2024-10-22 17:07:38 -0400

    reverse annotation order

--- batch/batch/front_end/front_end.py
+++ batch/batch/front_end/front_end.py
@@ -253,11 +253,12 @@ async def rest_cloud(_) -> APIResponse[str, Literal[200]]:
     return as_api_response(web.Response(text=CLOUD))
 
 
+@auth.authenticated_users_only()
 @SCHEMA.api()
 @routes.get('/api/v1alpha/supported_regions')
-@auth.authenticated_users_only()
-async def rest_get_supported_regions(request: web.Request, _) -> APIResponse[List[str], Literal[200]]:
-    return as_api_response(json_response(list(request.app['regions'].keys())))
+async def rest_get_supported_regions(request: web.Request, _) -> APIResponse[list[str], Literal[200]]:
+    return APIResponse(['foo', 'bar'])
+    # return as_api_response(json_response(list(request.app['regions'].keys())))
 
 
 async def _handle_ui_error(
